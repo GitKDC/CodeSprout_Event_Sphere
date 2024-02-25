cf592964501c5eefe1e8581fb2164401173e14a3
GitKDC <kartikchaudhari246@gmail.com>
2024-02-25 10:45:41 +0530

    hi

---
+++ Event_sphereFinal/index.php
@@ -0,0 +1,137 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>EVENT_SPHERE</title>
+    <link
+    href="https://cdn.jsdelivr.net/npm/remixicon@4.1.0/fonts/remixicon.css"
+    rel="stylesheet"
+    />
+    <link rel="stylesheet" href="style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Anta&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Black+Ops+One&family=Dancing+Script:wght@400..700&family=Josefin+Sans:ital,wght@0,100..700;1,100..700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@400..700&family=Josefin+Sans:ital,wght@0,100..700;1,100..700&display=swap" rel="stylesheet">
+</head>
+<body>
+    <div class="main">
+        <img class="bgimg" src="./bgimg.jpg"></img>
+        <div class="content">
+            <div class="upper">
+                <div class="nav">
+                    <img style="height: 2.9vw; width: 4.3%;" src="./logo.webp" alt="">
+                    <h4>GHRIEBM JALGAON.</h4>
+                    <ul class="list">
+                        <li id="li1"><a href="#">Home</a></li>
+                        <li id="li2"><a href="./Agnethon/index.php">Events</a></li>
+                        <li id="li3"><a href="#">Gallery</a></li>
+                        <!-- <li id="li4"><a href="#">Venue</a></li> -->
+                        <!-- <li id="li5"><a href="#"><button>Login</button></a></li> -->
+                    </ul>
+                    <button style="height: 2vw; width: 5.5vw;color:white" id="button-87" role="button">
+                      <a href="login.php" class="link signup-link">
+                      Login<i class="ri-arrow-right-line"></i></a></button>
+                    <!-- HTML !-->
+                </div>
+            </div>
+            <div class="title">
+                <h1>EVENT_SPHERE</h1>
+                <h5>Your one-stop Hub for Events, Approvals and Student Connectivity</h5>
+            </div>
+            <div class="frame">
+                <div class="box">
+                  <img src="./Slider/photo-1533174072545-7a4b6ad7a6c3.avif">
+                </div>
+                <div class="box">
+                  <img src="./Slider/traditional.jpeg">
+                  <!-- <div class="text">Shivjaynti</div>
+                  <div class="text">Mahotsav</div> -->
+                </div>
+                <div class="box">
+                  <img src="./Slider/seminar.jpg">
+                  <!-- <div style="color: white; position: absolute; ;">ROCK NIGHT</div> -->
+                </div>
+                <div class="box">
+                  <img src="./Slider/codingcomp.jpg">
+                </div>
+              </div>
+            <div class="center">
+                <h1>EXPLORE MORE EVENTS</h1>
+                <!-- <h5>Be part of a thriving community that celebrates the diversity of campus life. Connect,<br> engage, and make the most of your college experience with Event_sphere</h5> -->
+                <button class="bt">Click Here</button>
+            </div>
+        </div>
+    </div>
+    <div style="background-color: rgb(67, 67, 67);" id="down">
+        <div style="padding-left: 2vw; padding-top: 1.2vw; padding-bottom: 1.2vw; font-size: 2vw; color: rgb(255, 145, 0) ;" class="upc"><h2>Upcoming Events:</h2></div>
+        <hr style="border: 2px solid;">
+            <div style="display: flex;" class="up-events">
+              <div>
+                <h4 style="font-size: 1.2vw; padding-left: 6vw; padding-top: 2vw; color: white ;">Wed, 28 Feb</h4>
+                <h1 style="padding-left: 6vw; padding-bottom: 2vw; color: white;">Raisoni Shark Tank</h1>
+              </div>
+              <div style="margin-left: 10vw; margin-top: 3vw;"><button style="
+                padding: 0.5vw 1.1vw;
+                text-align: center;
+                text-transform: uppercase;
+                transition: 0.5s;
+                background-size: 200% auto;
+                color: white;
+                border-radius: 10px;
+                display: block;
+                border: 0px;
+                font-weight: 700;
+                box-shadow: 0px 0px 14px -7px #f09819;
+                background-image: linear-gradient(45deg, rgb(255, 145, 0) 0%, #F09819  51%, #FF512F  100%);
+                cursor: pointer;
+                user-select: none;
+                -webkit-user-select: none;
+                touch-action: manipulation;
+              ">Know more</button></div>
+            </div>
+            <hr style="border: 2px solid;">
+            <div style="display: flex;" class="up-events">
+              <div>
+                <h4 style="font-size: 1.2vw; padding-left: 6vw; padding-top: 2vw; color: white ;">Thur, 29 Feb</h4>
+                <h1 style="padding-left: 6vw; padding-bottom: 2vw; color: white;">Rock on Night</h1>
+              </div>
+              <div style="margin-left: 10vw; margin-top: 3vw;"><button style="
+                padding: 0.5vw 1.1vw;
+                text-align: center;
+                text-transform: uppercase;
+                transition: 0.5s;
+                background-size: 200% auto;
+                color: white;
+                border-radius: 10px;
+                display: block;
+                border: 0px;
+                font-weight: 700;
+                box-shadow: 0px 0px 14px -7px #f09819;
+                background-image: linear-gradient(45deg, rgb(255, 145, 0) 0%, #F09819  51%, #FF512F  100%);
+                cursor: pointer;
+                user-select: none;
+                -webkit-user-select: none;
+                touch-action: manipulation;
+              ">Know more</button></div>
+            </div>
+            <hr style="border: 2px solid;">
+            <div class="up-events">
+              <h4 style="font-size: 1.2vw; padding-left: 6vw; padding-top: 2vw; color: white ;">Fri, 1 Mar</h4>
+              <h1 style="padding-left: 6vw; padding-bottom: 2vw; color: white;">Personality Contest</h1>
+            </div>
+            <hr style="border: 2px solid;">
+            <div class="up-events">
+              <h4 style="font-size: 1.2vw; padding-left: 6vw; padding-top: 2vw; color: white ;">Sat, 2 Mar</h4>
+              <h1 style="padding-left: 6vw; padding-bottom: 2vw; color: white;">Annual Gathering</h1>
+            </div>
+            <hr style="border: 2px solid;">
+        </div>
+    </div>
+    <div class="footer">
+
+    </div>
+    <!-- <script src="./script.js"></script> -->
+</body>
+</html>
